92cd33646e1d64a07c8ef8d425e092dfc29898d9
ohs342 <ohs342@naver.com>
2018-07-10 02:03:39 +0900

    2018.07.10 by kimsuho comment

--- app/src/main/java/com/example/ryan/xmlparsing/MainActivity.java
+++ app/src/main/java/com/example/ryan/xmlparsing/MainActivity.java
@@ -25,13 +25,13 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_search, btn_search_gbis;
-    EditText ed_mStation_id;
-    private ListView mListView_Stations;
-    private ListView mListView_Bus;
+    Button btn_search, btn_search_gbis; //첫 화면의 검색버튼, 정류소번호 찾기 버튼 ( * 정류소번호 찾기 사이트 : www.gbis.go.kr )
+    EditText ed_mStation_id;  // 정류소번호를 입력하는 텍스트박스
+    private ListView mListView_Stations; // 입력한 정류소번호에 대한 정류소를 보여주는 리스트뷰
+    private ListView mListView_Bus;  // 정류소 중 내리고자하는 정류소를 클릭하고, 그 정류소를 운행하는 모든 버스들이 로드되는 리스트뷰
 
-    final StationAdapter mStationAdapter = new StationAdapter();
-    final RouteAdapter mRouteAdapter = new RouteAdapter();
+    final StationAdapter mStationAdapter = new StationAdapter(); // 위의 mListView_Stations 데이터를 받아오기 위한 어뎁터 선언
+    final RouteAdapter mRouteAdapter = new RouteAdapter(); // 위의 mListView_Bus 데이터를 받아오기 위한 어뎁터 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +41,10 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        ed_mStation_id = findViewById(R.id.edit_mStationId);
-        btn_search_gbis = findViewById(R.id.btn_search_gbis);
-        btn_search = findViewById(R.id.btn_search);
-        mListView_Stations = findViewById(R.id.ListView_Stations);
+        ed_mStation_id = findViewById(R.id.edit_mStationId); // 정류소번호 검색 텍스트박스 가져오기
+        btn_search_gbis = findViewById(R.id.btn_search_gbis); // 정류소번호 검색을 위한 버튼 아이디 가져오기
+        btn_search = findViewById(R.id.btn_search); // 입력한 정류소 번호로 해당 정류소번호에따른 정류장을 가져오기위한 검색버튼 가져오기
+        mListView_Stations = findViewById(R.id.ListView_Stations); // 정류장 리스트를 보여주는 리스트 뷰 가져오기
 
         btn_search_gbis.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,17 +59,17 @@ public class MainActivity extends AppCompatActivity {
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mStationId = ed_mStation_id.getText().toString();
+                String mStationId = ed_mStation_id.getText().toString();// 입력한 정류소 번호를 가져와서 String 변수에 저장한다.
 
-                try {
+                try {  // 저장된 변수를 공공데이터 포털 서버에 요청메시지로 보내어, 입력한 정류소번호에 해당하는 정류소정보를 가져온다.
                     String uri = "http://openapi.gbis.go.kr/ws/rest/busstationservice?serviceKey=i%2FmgmkmoCSBv8EUR8Jv1%2FTOw767UUNZEI%2FSGQnCmnDSb4kM1Vty5Dsqlw%2Bcx%2B8o%2FtfNUzA7PNyaMnqVHCMqD8A%3D%3D&keyword=" + mStationId;
                     Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(uri);
-                    XPath xpath = XPathFactory.newInstance().newXPath();
+                    XPath xpath = XPathFactory.newInstance().newXPath(); // xPath 를 사용하여 원하는 데이터를 분류하여 가져온다.
                     NodeList StationNameList = (NodeList) xpath.evaluate("//busStationList/stationName", document, XPathConstants.NODESET);
                     NodeList RegionNameList = (NodeList) xpath.evaluate("//busStationList/regionName", document, XPathConstants.NODESET);
                     NodeList StationIdList = (NodeList) xpath.evaluate("//busStationList/stationId", document, XPathConstants.NODESET);
                     for (int i = 0; i < StationNameList.getLength(); i++) {
-                        mStationAdapter.addItem(
+                        mStationAdapter.addItem( // 가져와서 데이터를 저장
                                 StationNameList.item(i).getTextContent(),
                                 RegionNameList.item(i).getTextContent() + "시",
                                 StationIdList.item(i).getTextContent()
@@ -95,7 +95,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 mListView_Bus = findViewById(R.id.ListView_Stations);
-                String selectedStationId = mStationAdapter.getItem(position).StationId;
+                String selectedStationId = mStationAdapter.getItem(position).StationId; // stationid(정류소번호)에 대한 정보를 담고있는 리스트를 클릭하여 해당정보를 공공데이터 포털 서버에 보내어
+                                                                                            // 해당 정류장을 지나가는 버스리스트를 로드한다
                 String uri = "http://openapi.gbis.go.kr/ws/rest/busstationservice/route?serviceKey=" +
                         "i%2FmgmkmoCSBv8EUR8Jv1%2FTOw767UUNZEI%2FSGQnCmnDSb4kM1Vty5Dsqlw%2Bcx%2B8o%2FtfNUzA7PNyaMnqVHCMqD8A%3D%3D&" +
                         "stationId=" + selectedStationId;
--- app/src/main/java/com/example/ryan/xmlparsing/RouteInfo.java
+++ app/src/main/java/com/example/ryan/xmlparsing/RouteInfo.java
@@ -1,9 +1,9 @@
 package com.example.ryan.xmlparsing;
 
 public class RouteInfo {
-    public String routeId;
-    public String busNumber;
-    public String busTypeName;
+    public String routeId; // 특정 버스에 고유적으로 부여되는 노선아이디를 받아오기 위한 변수
+    public String busNumber;  // 버스번호
+    public String busTypeName;  // 버스의 타입을 받아오는 변수(광역, 일반 등등)
 
     public RouteInfo(String routeId, String busNumber, String busTypeName) {
         this.routeId = routeId;
