5c5180bc53468c6d67fda4298d5acb70d9c5aac4
ohs342 <ohs342@naver.com>
2018-07-10 02:05:21 +0900

    2018.07.10 by kimsuho comment

--- app/src/main/java/com/example/ryan/xmlparsing/ResultStationInfo.java
+++ app/src/main/java/com/example/ryan/xmlparsing/ResultStationInfo.java
@@ -1,8 +1,8 @@
 package com.example.ryan.xmlparsing;
 
 public class ResultStationInfo {
-    String stationName;
-    String stationSeq;
+    String stationName; // 정류장 이름
+    String stationSeq;  // 정류장의 순서
 
     ResultStationInfo(String stationName, String stationSeq){
         this.stationName = stationName;
