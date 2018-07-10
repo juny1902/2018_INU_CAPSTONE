1fceebafc8204f55318189a87ec281e564586588
juny1905 <juny1905@naver.com>
2018-07-10 17:46:31 +0900

    Set Station Marker

--- app/src/main/java/com/example/ryan/xmlparsing/ResultStationAdapter.java
+++ app/src/main/java/com/example/ryan/xmlparsing/ResultStationAdapter.java
@@ -15,6 +15,13 @@ import java.util.ArrayList;
 public class ResultStationAdapter extends BaseAdapter {
     private ArrayList<ResultStationInfo> mItems = new ArrayList<>();
 
+    public class CustomViewHolder {
+        public TextView tv_plateNumber;
+        public TextView tv_station_name;
+        public TextView tv_sequence_number;
+        public ImageView im_current_bus;
+    }
+
     @Override
     public int getCount() {
         return mItems.size();
@@ -34,34 +41,42 @@ public class ResultStationAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         Context context = parent.getContext();
 
+        CustomViewHolder holder;
+        holder = new CustomViewHolder();
         /* 'listview_custom' Layout을 inflate하여 convertView 참조 획득 */
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.listview_result, parent, false);
+            convertView.setTag(holder);
         }
 
         /* 'listview_custom'에 정의된 위젯에 대한 참조 획득 */
-        TextView tv_plateNumber = convertView.findViewById(R.id.tv_busPlateNumber);
-        TextView tv_station_name = convertView.findViewById(R.id.tv_station_name);
-        TextView tv_sequence_number = convertView.findViewById(R.id.tv_sequence_number);
-        ImageView im_current_bus = convertView.findViewById(R.id.im_current_bus);
+        holder.tv_plateNumber = convertView.findViewById(R.id.tv_busPlateNumber);
+        holder.tv_station_name = convertView.findViewById(R.id.tv_station_name);
+        holder.tv_sequence_number = convertView.findViewById(R.id.tv_sequence_number);
+        holder.im_current_bus = convertView.findViewById(R.id.im_current_bus);
+
+
+
         if(mItems.get(position).isSelected){
-            tv_station_name.setTextColor(Color.RED);
+            convertView.setBackgroundColor(Color.GRAY);
+        }else{
+            convertView.setBackgroundColor(Color.TRANSPARENT);
         }
         if(mItems.get(position).visibility){
-            im_current_bus.setVisibility(View.VISIBLE);
-            tv_plateNumber.setVisibility(View.VISIBLE);
+            holder.im_current_bus.setVisibility(View.VISIBLE);
+            holder.tv_plateNumber.setVisibility(View.VISIBLE);
         }else{
-            im_current_bus.setVisibility(View.INVISIBLE);
-            tv_plateNumber.setVisibility(View.INVISIBLE);
+            holder.im_current_bus.setVisibility(View.INVISIBLE);
+            holder.tv_plateNumber.setVisibility(View.INVISIBLE);
         }
         /* 각 리스트에 뿌려줄 아이템을 받아오는데 mMyItem 재활용 */
         ResultStationInfo myItem = getItem(position);
 
         /* 각 위젯에 세팅된 아이템을 뿌려준다 */
-        tv_station_name.setText(myItem.stationName);
-        tv_sequence_number.setText(myItem.stationSeq);
-        tv_plateNumber.setText(myItem.currentPlateNumber);
+        holder.tv_station_name.setText(myItem.stationName);
+        holder.tv_sequence_number.setText(myItem.stationSeq);
+        holder.tv_plateNumber.setText(myItem.currentPlateNumber);
 
         /* (위젯에 대한 이벤트리스너를 지정하고 싶다면 여기에 작성하면된다..)  */
         return convertView;
