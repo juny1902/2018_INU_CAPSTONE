3f0f0fa5b21bffce1ed5e2258f3b58aa6312a4fa
juny1905 <juny1905@naver.com>
2018-07-10 11:49:32 +0900

    180710 - Backup Point 1

--- app/src/main/java/com/example/ryan/xmlparsing/RouteListActivity.java
+++ app/src/main/java/com/example/ryan/xmlparsing/RouteListActivity.java
@@ -27,24 +27,33 @@ public class RouteListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        // 이전 인텐트에서 routeId 및 busNumber 가져옴.
         Intent curIntent = getIntent();
         String routeId = curIntent.getStringExtra("routeId");
         String busNumber = curIntent.getStringExtra("busNumber");
 
+        // 뷰와 객체 연결
         btn_ride = findViewById(R.id.btn_ride);
         btn_off = findViewById(R.id.btn_off);
         mListView = findViewById(R.id.ListView_Results);
         mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
+        // 쿼리문 지정 (Route ID 이용)
         String uri = "http://openapi.gbis.go.kr/ws/rest/busrouteservice/station?" +
                 "serviceKey=i%2FmgmkmoCSBv8EUR8Jv1%2FTOw767UUNZEI%2FSGQnCmnDSb4kM1Vty5Dsqlw%2Bcx%2B8o%2FtfNUzA7PNyaMnqVHCMqD8A%3D%3D&" +
                 "routeId="+routeId;
         Document document = null;
         try {
+            // 문서 초기화
             document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(uri);
             XPath xpath = XPathFactory.newInstance().newXPath();
+
+            // XML 문서에서 필요한 정보 받아옴
             NodeList stationNameList = (NodeList) xpath.evaluate("//busRouteStationList/stationName", document, XPathConstants.NODESET);
             NodeList stationSeqList = (NodeList) xpath.evaluate("//busRouteStationList/stationSeq", document, XPathConstants.NODESET);
+
+            // 어댑터에 Parsing 한 정보 넣기
             for (int i = 0; i < stationNameList.getLength(); i++) {
                 mResultStationAdapter.addItem(
                         stationNameList.item(i).getTextContent(),
@@ -52,6 +61,7 @@ public class RouteListActivity extends AppCompatActivity {
                 );
             }
 
+            // 리스트뷰에 어댑터 접속
             mListView.setAdapter(mResultStationAdapter);
         } catch (SAXException e) {
             e.printStackTrace();
